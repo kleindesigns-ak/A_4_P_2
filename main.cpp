059ac1b08676067a0331403ed94e161a9846c60d
Austin Klein <austinwklein@gmail.com>
2022-11-26 20:26:05 -0900

    Sentinel game loop functional

--- main.cpp
+++ main.cpp
@@ -12,5 +12,20 @@ using namespace std;
 int main() {
     // Initialize the world with 5 doodlebugs and 100 ants in random locations
 
+
+    cout << endl << "Welcome to the Doodlebug Simulator " << endl << endl << "Have you ever wanted to see how doodlebugs attack ants in the " << endl << "wild? Well sit back and get ready to watch a time-lapsed " << endl << "simulation of the ant vs doodlebug life cycle. " << endl << endl;
+
+    char gameLoopSentinel;
+    do {
+        // Flush the buffer from the last loop
+        cin.ignore();
+
+
+
+        // Sentinel Loop Control for advancing to the next time step
+        cout << "Press 'Enter' to advance another time step. " << endl;
+        cin.get(gameLoopSentinel);
+    } while ( gameLoopSentinel == '\n' );
+
     return 0;
 }
